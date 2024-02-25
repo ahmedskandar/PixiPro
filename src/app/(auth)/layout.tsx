33c09ce5b278de482aafed1bf6f3b1e143415dfc
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-02-25 16:58:41 +0300

    feat: Implement clerk authentication

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,7 @@
 import React, { PropsWithChildren } from "react";
 
 const Layout = ({ children }: PropsWithChildren) => {
-  return <main>Layout</main>;
+  return <main className="auth">{children}</main>;
 };
 
 export default Layout;
--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,5 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1>Home</h1>
+      <UserButton />
+    </div>
+  );
 }
