3f681d1102f695278addf0f584134a76723fde06
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-02-26 18:14:07 +0300

    feat: Implement logo component

---
+++ src/components/header/Logo.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <div className="md:absolute flex text-customBlue md:top-1/2 md:left-1/2 md:transform md:-translate-x-1/2 md:-translate-y-1/2">
+      <span className="font-bold text-2xl md:text-4xl font-mono ">PixiPoo</span>
+    </div>
+  );
+};
+
+export default Logo;
