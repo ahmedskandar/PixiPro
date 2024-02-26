42480e1c482bae50d2e79367b1a1d41be8778c0d
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-02-26 19:18:36 +0300

    feat: Implement db models

---
+++ src/lib/database/models/image.model.ts
@@ -0,0 +1,42 @@
+import { Schema, model, models } from "mongoose";
+
+export type IImage = Document & {
+  title: string;
+  transformationType: string;
+  publicId: string;
+  secureURL: string;
+  width?: number;
+  height?: number;
+  config?: object;
+  transformationUrl?: string;
+  aspectRatio?: string;
+  color?: string;
+  prompt?: string;
+  author: {
+    _id: string;
+    firstName: string;
+    lastName: string;
+  };
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const ImageSchema = new Schema({
+  title: { type: String, required: true },
+  transformationType: { type: String, required: true },
+  publicId: { type: String, required: true },
+  secureUrl: { trype: String, required: true },
+  width: { trype: Number },
+  height: { type: Number },
+  config: { type: Object },
+  transformationUrl: { type: String },
+  color: { type: String },
+  prompt: { type: String },
+  author: { type: Schema.Types.ObjectId, ref: "User" },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+});
+
+const Image = models?.Image || model("Image", ImageSchema);
+
+export default Image;
